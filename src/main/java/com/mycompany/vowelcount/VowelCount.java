8ce98b3f15101bb927337545c6eed9e613558063
RC_Student_lab <RC_Student_lab@10.0.1.23>
2024-10-16 06:47:04 +0000

    comments added

--- src/main/java/com/mycompany/vowelcount/VowelCount.java
+++ src/main/java/com/mycompany/vowelcount/VowelCount.java
@@ -17,7 +17,7 @@ public class VowelCount {
         //we prompt the user to enter the string of their choice
         System.out.println("Enter a sentence:");
         String input = scanner.nextLine();
-        
+        //here we display the number of vowels
         int vowelCount = countVowels(input);
         System.out.println("Number of vowels: " + vowelCount);
         
@@ -29,7 +29,7 @@ public class VowelCount {
         sentence = sentence.toLowerCase();
         int count = 0;
 
-        
+        //we use a loop to go through the given sentense or string to check for vowels
         for (int i = 0; i < sentence.length(); i++) {
             char currentChar = sentence.charAt(i);
             // Check if the character is a vowel
