b636eb6a253de9e10a446a0ba1b7b6d4c92eb15c
RC_Student_lab <RC_Student_lab@10.0.1.23>
2024-10-16 06:44:22 +0000

    2nd commit is on converting the string to all lowercase letters

--- src/main/java/com/mycompany/vowelcount/VowelCount.java
+++ src/main/java/com/mycompany/vowelcount/VowelCount.java
@@ -11,13 +11,10 @@ package com.mycompany.vowelcount;
  import java.util.Scanner;
 public class VowelCount {
 
-   
-
-
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+        //we prompt the user to enter the string of their choice
         System.out.println("Enter a sentence:");
         String input = scanner.nextLine();
         
@@ -28,14 +25,14 @@ public class VowelCount {
     }
 
     public static int countVowels(String sentence) {
-       
-     
+        //here we convert the given the string to all lowercase letters
+        sentence = sentence.toLowerCase();
         int count = 0;
 
         
         for (int i = 0; i < sentence.length(); i++) {
             char currentChar = sentence.charAt(i);
-            //here we check if the characters that make up the string are vowels
+            // Check if the character is a vowel
             if ("aeiou".indexOf(currentChar) != -1) {
                 count++;
             }
@@ -44,5 +41,7 @@ public class VowelCount {
         return count;
     }
 }
+
+
 
 
